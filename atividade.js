dc09322aaa7860011a966d12df470afc757cd6e6
biapagotto <biapagotto.ux@gmail.com>
2024-08-19 17:23:33 -0300

    first commit

---
+++ atividade.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiply(a, b) {
+    return a * b;
+}
+function greet(name) {
+    return `Olá ${name}`;
+}
+const result = multiply(5, 6);
+const greeting = greet("Ana");
+console.log(result);
+console.log(greeting);
---
+++ atividade.ts
@@ -0,0 +1,13 @@
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function greet(name: string): string {
+    return `Olá ${name}`;
+}
+
+const result = multiply(5, 6); 
+const greeting = greet("Ana"); 
+
+console.log(result); 
+console.log(greeting);
